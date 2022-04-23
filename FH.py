d74712db7201a9f1129ac22160df814182081afc
GrapeKing <46871864+GrapeKing@users.noreply.github.com>
2022-04-23 14:02:02 +0100

    Add files via upload

--- FH.py
+++ FH.py
@@ -14,7 +14,8 @@ def help():
 	print('	critical(n = 1): returns the critical concentration and critical interaction [phi_c, chi_c]\n')
 	print('	spinodal(chi, n = 1): returns spinodal concentrations [p1, p2, chi] in the valid chi range\n')
 	print('	GL_binodal(chi, n = 1): Ginzburg-Landau binodal [p1, p2, chi]\n')
-	print('	binodal(chi, n = 1, iteration = 5, UseImprovedMap = True): self-consistent solution with speficied number of iterations [p1, p2, chi]. You can also use the simple map to see what it does.')
+	print('	binodal(chi, n = 1, iteration = 5, UseImprovedMap = True): self-consistent solution with speficied number of iterations [p1, p2, chi]. You can also use the simple map to see what it does\n')
+	print(' analytic_binodal(x, n = 1, UseImprovedMap = True): analytic forms')
 
 def critical(n = 1):
 
@@ -149,3 +150,75 @@ def binodal(x, n = 1, iteration = 5, UseImprovedMap = True):
 				p2 = p2_new
 
 		return np.array([p1, p2, xx])
+
+def analytic_binodal(x, n = 1, UseImprovedMap = True):
+
+	crit = critical(n)
+	x_c = crit[1]
+	phi_c = crit[0]
+	gamma = 1. - 1./n
+
+	Delta = 0.185
+
+	if n == 1:
+
+		guess = GL_binodal(x)
+		
+		pm = guess[1]
+		xx = guess[2]
+		
+		if UseImprovedMap:
+
+			p0 = 0.5 * (pm + np.sqrt(pm ** 2 + Delta ** 2))
+			ee = np.exp(- 2 * xx * p0 + xx)
+			pm = 1/(1 + ee)
+
+		else:
+
+			p0 = 0.5 * (pm + np.abs(pm))
+			ee = np.exp(- 2 * xx * p0 + xx)
+			pm = 1/(1 + ee)
+
+		return np.array([1 - pm, pm, xx])
+
+	if n > 1:
+
+		guess = GL_binodal(x, n = n)
+		
+		pm = guess[1]
+		xx = guess[2]
+
+		root = np.sqrt(6 * (xx - x_c) /(x_c ** 2 * np.sqrt(n)))
+		numerator = 1 - np.exp(-root * (gamma + 2 * xx * phi_c))
+		denominator = 1 - np.exp(-root * (2 * xx * n *(1 - gamma * phi_c) - n * gamma ** 2))
+
+		pp = numerator / denominator
+
+		if UseImprovedMap:
+
+			p0 = 0.5 * (pm + np.sqrt(pm ** 2 + Delta ** 2 / n))
+	
+			a = np.exp( - 2. * xx * (pp - p0))
+			b = np.exp( - gamma * (pp - p0) - xx * (np.power(pp,2) - np.power(p0,2)))
+			c = np.power(a/b, n)
+	
+			g1 = (1. - b)/(1. - np.power(a/b, n) * b)
+			g2 = (1. - b)/(np.power(b/a, n) - b)
+
+			pm = np.copy((1. - b)/(np.power(b/a, n) - b))
+
+
+		else:
+
+			p0 = 0.5 * (pm + np.abs(pm))
+	
+			a = np.exp( - 2. * xx * (pp - p0))
+			b = np.exp( - gamma * (pp - p0) - xx * (np.power(pp,2) - np.power(p0,2)))
+			c = np.power(a/b, n)
+	
+			g1 = (1. - b)/(1. - np.power(a/b, n) * b)
+			g2 = (1. - b)/(np.power(b/a, n) - b)
+
+			pm = np.copy((1. - b)/(np.power(b/a, n) - b))
+
+		return np.array([pp, pm, xx])
