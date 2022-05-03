3928eccee6ab150b7bba03ec45e3d63af8e7d9b0
KnowlesLab-Cambridge <67645971+KnowlesLab-Cambridge@users.noreply.github.com>
2022-05-03 20:23:51 +0100

    updated analytic functions

--- FH.py
+++ FH.py
@@ -151,74 +151,44 @@ def binodal(x, n = 1, iteration = 5, UseImprovedMap = True):
 
 		return np.array([p1, p2, xx])
 
-def analytic_binodal(x, n = 1, UseImprovedMap = True):
+def analytic_binodal(x, n = 1):
 
 	crit = critical(n)
 	x_c = crit[1]
 	phi_c = crit[0]
 	gamma = 1. - 1./n
 
-	Delta = 0.185
-
 	if n == 1:
 
 		guess = GL_binodal(x)
-		
+
 		pm = guess[1]
 		xx = guess[2]
 		
-		if UseImprovedMap:
-
-			p0 = 0.5 * (pm + np.sqrt(pm ** 2 + Delta ** 2))
-			ee = np.exp(- 2 * xx * p0 + xx)
-			pm = 1/(1 + ee)
-
-		else:
-
-			p0 = 0.5 * (pm + np.abs(pm))
-			ee = np.exp(- 2 * xx * p0 + xx)
-			pm = 1/(1 + ee)
+		p0 = 0.5 * (pm + np.abs(pm))
+		ee = np.exp(- 2 * xx * p0 + xx)
+		pm = 1/(1 + ee)
 
 		return np.array([1 - pm, pm, xx])
 
 	if n > 1:
 
 		guess = GL_binodal(x, n = n)
-		
+
+		pp = guess[0]
 		pm = guess[1]
 		xx = guess[2]
 
-		root = np.sqrt(6 * (xx - x_c) /(x_c ** 2 * np.sqrt(n)))
-		numerator = 1 - np.exp(-root * (gamma + 2 * xx * phi_c))
-		denominator = 1 - np.exp(-root * (2 * xx * n *(1 - gamma * phi_c) - n * gamma ** 2))
-
-		pp = numerator / denominator
-
-		if UseImprovedMap:
-
-			p0 = 0.5 * (pm + np.sqrt(pm ** 2 + Delta ** 2 / n))
+		p0 = 0.5 * (pm + np.abs(pm))
 	
-			a = np.exp( - 2. * xx * (pp - p0))
-			b = np.exp( - gamma * (pp - p0) - xx * (np.power(pp,2) - np.power(p0,2)))
-			c = np.power(a/b, n)
+		a = np.exp( - 2. * xx * (pp - p0))
+		b = np.exp( - gamma * (pp - p0) - xx * (np.power(pp,2) - np.power(p0,2)))
+		c = np.power(a/b, n)
 	
-			g1 = (1. - b)/(1. - np.power(a/b, n) * b)
-			g2 = (1. - b)/(np.power(b/a, n) - b)
+		g1 = (1. - b)/(1. - np.power(a/b, n) * b)
+		g2 = (1. - b)/(np.power(b/a, n) - b)
 
-			pm = np.copy((1. - b)/(np.power(b/a, n) - b))
-
-
-		else:
-
-			p0 = 0.5 * (pm + np.abs(pm))
-	
-			a = np.exp( - 2. * xx * (pp - p0))
-			b = np.exp( - gamma * (pp - p0) - xx * (np.power(pp,2) - np.power(p0,2)))
-			c = np.power(a/b, n)
-	
-			g1 = (1. - b)/(1. - np.power(a/b, n) * b)
-			g2 = (1. - b)/(np.power(b/a, n) - b)
-
-			pm = np.copy((1. - b)/(np.power(b/a, n) - b))
+		pm = np.copy((1. - b)/(np.power(b/a, n) - b))
+		pp = np.copy((1. - b)/(1 - np.power(a/b, n) * b))
 
 		return np.array([pp, pm, xx])
